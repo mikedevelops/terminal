26f1e228f36e9a74b0da81533a6d4e6f0455e6a2
Michael Smart <michaelsmart88@gmail.com>
2016-11-06 13:47:40 +0000

    added some basic link, styling;

--- src/javascript/helpers/terminal.js
+++ src/javascript/helpers/terminal.js
@@ -5,8 +5,29 @@ export default {
      * Print text line
      * @return  {JSX}
      */
-    printText (content) {
-        return <p className="terminal__text">{ content }</p>
+    printText (content, tag = 'p', link = false) {
+        const className = 'terminal__text'
+        let output
+
+        switch (tag) {
+        case 'p':
+            output = (
+                <p className={className}>{ content }</p>
+            )
+            break
+        case 'a':
+            output =  (
+                <p className={className}>
+                    { content }
+                    <a href={link} className={className + ' terminal__link'}>
+                        { link }
+                    </a>
+                </p>
+            )
+            break
+        }
+
+        return output
     },
 
     /**
